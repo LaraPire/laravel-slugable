fc97badc17d245cb6b2e0f0865940ec852581914
Rayiumir <swordart2050@gmail.com>
2025-04-05 20:11:50 +0330

    Added SlugServiceProvider

--- src/HasSlug.php
+++ src/HasSlug.php
@@ -1,31 +1,10 @@
 <?php
 
-use Illuminate\Support\Str;
-trait HasSlug
+namespace Rayiumir\HasSlug;
+class HasSlug
 {
-    /**
-     * Boot the trait.
-     *
-     * @return void
-     */
-    public static function HasSlug()
+    public static function index(): string
     {
-        static::saving(function ($model) {
-            $model->generateSlug();
-        });
-    }
-
-    /**
-     * Generate the slug from the specified field.
-     *
-     * @return void
-     */
-    public function generateSlug(): void
-    {
-        $slugSource = $this->slugSourceField ?? 'title'; // Default to 'title' if not defined
-        $slugDestination = $this->slugDestinationField ?? 'slug'; // Default to 'slug' if not defined
-
-        // Generate the slug and assign it to the destination field
-        $this->{$slugDestination} = Str::slug($this->{$slugSource});
+        return "Hello World";
     }
 }
---
+++ src/ServiceProvider/SlugServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ServiceProvider;
+
+use Illuminate\Support\ServiceProvider;
+use Rayiumir\HasSlug\HasSlug;
+
+class SlugServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->app->bind('HasSlug', function() {
+            return new HasSlug();
+        });
+    }
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->_loadPublished();
+    }
+    private function _loadPublished(): void
+    {
+        $this->publishes([
+            __DIR__.'/../Traits' => app_path('Traits/')
+        ],'LaravelHasSlug');
+    }
+}
