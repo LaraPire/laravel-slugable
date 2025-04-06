566bb23a3a5a5ca26d2a25a446ba754bdb6ee7d8
Rayiumir <swordart2050@gmail.com>
2025-04-07 00:00:17 +0330

    Fixed Slugable

--- src/Traits/HasSlugable.php
+++ src/Traits/HasSlugable.php
@@ -8,10 +8,8 @@ use Illuminate\Support\Str;
 trait HasSlugable
 {
     /**
-     * Boot the trait Slugable.
-     *
-     * @return void
-     */
+     * Boot the trait HasSlugable.
+     **/
     public static function bootHasSlugable(): void
     {
         static::saving(function (Model $model) {
@@ -19,39 +17,48 @@ trait HasSlugable
             $destination = $model->slugDestinationField ?? 'slug';
 
             if (empty($model->$destination) && !empty($model->$source)) {
-                $model->$destination = static::makeMultilingualSlug($model->$source);
+                $slug = static::generateSlugFrom($model->$source);
+                $model->$destination = static::makeSlugUnique($model, $slug, $destination);
             }
         });
     }
 
     /**
      * Supported From Persian and Arabic to translate slug
-     *
-     * @param string $string
-     * @return string
-     */
-    protected static function makeMultilingualSlug(string $string): string
+     **/
+
+    protected static function generateSlugFrom(string $value): string
     {
-        $transliterationMap = [
+        $value = static::convertToAscii($value);
+        return Str::slug($value);
+    }
 
-            // Persian and Arabic
+    protected static function convertToAscii(string $string): string
+    {
+        $map = [
+            'ا' => 'a', 'أ' => 'a', 'آ' => 'a', 'إ' => 'e', 'ب' => 'b', 'پ' => 'p', 'ت' => 't', 'ث' => 'th', 'ج' => 'j', 'چ' => 'ch',
+            'ح' => 'h', 'خ' => 'kh', 'د' => 'd', 'ذ' => 'dh', 'ر' => 'r', 'ز' => 'z', 'ژ' => 'zh', 'س' => 's', 'ش' => 'sh',
+            'ص' => 's', 'ض' => 'z', 'ط' => 't', 'ظ' => 'z', 'ع' => 'a', 'غ' => 'gh', 'ف' => 'f', 'ق' => 'gh', 'ک' => 'k',
+            'گ' => 'g', 'ل' => 'l', 'م' => 'm', 'ن' => 'n', 'و' => 'v', 'ه' => 'h', 'ی' => 'y', 'ي' => 'y', 'ئ' => 'y', 'ة' => 'h',
+            '‌' => '-',
 
-            'آ' => 'a', 'ا' => 'a', 'ب' => 'b', 'پ' => 'p', 'ت' => 't', 'ث' => 's', 'ج' => 'j',
-            'چ' => 'ch', 'ح' => 'h', 'خ' => 'kh', 'د' => 'd', 'ذ' => 'z', 'ر' => 'r', 'ز' => 'z',
-            'ژ' => 'zh', 'س' => 's', 'ش' => 'sh', 'ص' => 's', 'ض' => 'z', 'ط' => 't', 'ظ' => 'z',
-            'ع' => 'a', 'غ' => 'gh', 'ف' => 'f', 'ق' => 'gh', 'ک' => 'k', 'گ' => 'g', 'ل' => 'l',
-            'م' => 'm', 'ن' => 'n', 'و' => 'v', 'ه' => 'h', 'ی' => 'y', 'ء' => '', 'ئ' => 'y',
-            'ؤ' => 'v', 'ة' => 'h', 'ي' => 'y',
+            '۰' => '0', '۱' => '1', '۲' => '2', '۳' => '3', '۴' => '4', '۵' => '5', '۶' => '6', '۷' => '7', '۸' => '8', '۹' => '9',
+            '٠' => '0', '١' => '1', '٢' => '2', '٣' => '3', '٤' => '4', '٥' => '5', '٦' => '6', '٧' => '7', '٨' => '8', '٩' => '9'
         ];
 
-        $string = preg_replace('/[\p{Mn}\p{Pd}\p{Zs}]/u', ' ', $string);
+        return strtr($string, $map);
+    }
 
-        $slug = str_replace(array_keys($transliterationMap), array_values($transliterationMap), $string);
+    protected static function makeSlugUnique(Model $model, string $slug, string $column): string
+    {
+        $original = $slug;
+        $i = 1;
 
-        if (function_exists('transliterator_transliterate')) {
-            $slug = transliterator_transliterate('Any-Latin; Latin-ASCII', $slug);
+        while ($model->newQuery()->where($column, $slug)->exists()) {
+            $slug = $original . '-' . $i++;
         }
 
-        return Str::slug($slug);
+        return $slug;
     }
+
 }
