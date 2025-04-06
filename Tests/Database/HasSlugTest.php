a45498758df4e1830a557c6d58b3946ac3cde8e3
Rayiumir <swordart2050@gmail.com>
2025-04-06 04:30:27 +0330

    Chanages function HasSlug and Added Tests - Supported From Persian and Arabic to translate slug

---
+++ Tests/Database/HasSlugTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+use Rayiumir\HasSlug\Traits\HasSlug;
+
+class HasSlugTest
+{
+    public function test_slug_is_generated_automatically(): void
+    {
+        $post = new class extends Model {
+            use HasSlug;
+
+            protected $table = 'posts';
+            public $timestamps = false;
+
+            protected $fillable = ['title', 'slug'];
+        };
+
+        $post->title = 'hello world!';
+        $post->save();
+
+        $this->assertEquals('hello-world!', $post->slug);
+    }
+}
--- src/Traits/HasSlug.php
+++ src/Traits/HasSlug.php
@@ -2,33 +2,56 @@
 
 namespace Rayiumir\HasSlug\Traits;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 trait HasSlug
 {
     /**
-     * Boot the trait.
+     * Boot the trait HasSlug.
      *
      * @return void
      */
     public static function HasSlug(): void
     {
-        static::saving(function ($model) {
-            $model->generateSlug();
+        static::saving(function (Model $model) {
+            $source = $model->slugSourceField ?? 'title';
+            $destination = $model->slugDestinationField ?? 'slug';
+
+            if (empty($model->$destination) && !empty($model->$source)) {
+                $model->$destination = static::makeMultilingualSlug($model->$source);
+            }
         });
     }
 
     /**
-     * Generate the slug from the specified field.
+     * Supported From Persian and Arabic to translate slug
      *
-     * @return void
+     * @param string $string
+     * @return string
      */
-    public function generateSlug(): void
+    protected static function makeMultilingualSlug(string $string): string
     {
-        $slugSource = $this->slugSourceField ?? 'title'; // Default to 'title' if not defined
-        $slugDestination = $this->slugDestinationField ?? 'slug'; // Default to 'slug' if not defined
+        $transliterationMap = [
 
-        // Generate the slug and assign it to the destination field
-        $this->{$slugDestination} = Str::slug($this->{$slugSource});
+            // Persian and Arabic
+
+            'آ' => 'a', 'ا' => 'a', 'ب' => 'b', 'پ' => 'p', 'ت' => 't', 'ث' => 's', 'ج' => 'j',
+            'چ' => 'ch', 'ح' => 'h', 'خ' => 'kh', 'د' => 'd', 'ذ' => 'z', 'ر' => 'r', 'ز' => 'z',
+            'ژ' => 'zh', 'س' => 's', 'ش' => 'sh', 'ص' => 's', 'ض' => 'z', 'ط' => 't', 'ظ' => 'z',
+            'ع' => 'a', 'غ' => 'gh', 'ف' => 'f', 'ق' => 'gh', 'ک' => 'k', 'گ' => 'g', 'ل' => 'l',
+            'م' => 'm', 'ن' => 'n', 'و' => 'v', 'ه' => 'h', 'ی' => 'y', 'ء' => '', 'ئ' => 'y',
+            'ؤ' => 'v', 'ة' => 'h', 'ي' => 'y',
+        ];
+
+        $string = preg_replace('/[\p{Mn}\p{Pd}\p{Zs}]/u', ' ', $string);
+
+        $slug = str_replace(array_keys($transliterationMap), array_values($transliterationMap), $string);
+
+        if (function_exists('transliterator_transliterate')) {
+            $slug = transliterator_transliterate('Any-Latin; Latin-ASCII', $slug);
+        }
+
+        return Str::slug($slug);
     }
 }
